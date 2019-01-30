1a1ba12cbc2538b3e1adfb40a863d83a0f653aae
Ilgu Lee <iglee2000ca@gmail.com>
2019-01-30 17:38:26 -0500

    Initial commit

---
+++ app/src/main/java/ilgulee/com/resokotlinmvvm/ui/QuoteActivity.kt
@@ -0,0 +1,13 @@
+package ilgulee.com.resokotlinmvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ilgulee.com.resokotlinmvvm.R
+
+class QuoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote)
+    }
+}
